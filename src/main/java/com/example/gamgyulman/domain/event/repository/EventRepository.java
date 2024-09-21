50e35c7755ffaedc766e8b9b2037cd0af84e0d13
Jeongmo Seo <wjdah1402@gmail.com>
2024-09-21 23:30:29 +0900

    :bug: fix: 쿼리 수정

--- src/main/java/com/example/gamgyulman/domain/event/repository/EventRepository.java
+++ src/main/java/com/example/gamgyulman/domain/event/repository/EventRepository.java
@@ -10,13 +10,13 @@ public interface EventRepository extends JpaRepository<Event, Long> {
     @Query("SELECT CASE WHEN COUNT(e) > 0 THEN TRUE ELSE FALSE END " +
             "FROM Event e " +
             "WHERE e.dayEvents = :dayEvents " +
-            "AND (e.startTime BETWEEN :startTime AND :endOfStartTime " +
-            "OR e.endTime BETWEEN :endTime AND :endOfEndTime)")
+            "AND (" +
+            "(:startTime BETWEEN e.startTime AND e.endTime)" +
+            "OR (:endTime BETWEEN e.startTime AND e.endTime)" +
+            ")")
     boolean existsByDayEventsIsAndStartTimeIsBetweenOrEndTimeIsBetween(@Param("dayEvents") DayEvents dayEvents,
                                                                        @Param("startTime") String startTime,
-                                                                       @Param("endOfStartTime") String endOfStartTime,
-                                                                       @Param("endTime") String endTime,
-                                                                       @Param("endOfEndTime") String endOfEndTime);
+                                                                       @Param("endTime") String endTime);
 
     @Query("SELECT CASE WHEN COUNT(e) > 0 THEN TRUE ELSE FALSE END " +
             "FROM Event e " +
