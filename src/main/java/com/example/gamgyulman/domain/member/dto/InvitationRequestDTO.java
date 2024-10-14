389df219101c4b43e12b7d88956d005a3ce07d81
Jeongmo Seo <wjdah1402@gmail.com>
2024-10-14 16:28:10 +0900

    :sparkles: feat: 초대 DTO defaultValue로 형태 추가

--- src/main/java/com/example/gamgyulman/domain/member/dto/InvitationRequestDTO.java
+++ src/main/java/com/example/gamgyulman/domain/member/dto/InvitationRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.gamgyulman.domain.member.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 public class InvitationRequestDTO {
@@ -7,12 +8,14 @@ public class InvitationRequestDTO {
     @Getter
     public static class CreateInvitationDTO {
         private Long scheduleId;
+        @Schema(defaultValue = "test@email.com")
         private String receiverEmail;
     }
 
     @Getter
     public static class UpdateInvitationStatusDTO {
         private Long invitationId;
+        @Schema(defaultValue = "ACCEPT")
         private String status;
     }
 }
