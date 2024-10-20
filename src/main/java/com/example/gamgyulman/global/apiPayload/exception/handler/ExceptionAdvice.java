b1b5721217c612d1fc483c37e981a592a027e264
Jeongmo Seo <wjdah1402@gmail.com>
2024-10-20 14:15:35 +0900

    :bug: fix: Error Handler에 상태코드 추가

--- src/main/java/com/example/gamgyulman/global/apiPayload/exception/handler/ExceptionAdvice.java
+++ src/main/java/com/example/gamgyulman/global/apiPayload/exception/handler/ExceptionAdvice.java
@@ -7,6 +7,8 @@ import com.example.gamgyulman.global.apiPayload.exception.GeneralException;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -21,37 +23,37 @@ import java.util.Map;
 public class ExceptionAdvice {
 
     @ExceptionHandler(ConstraintViolationException.class)
-    public CustomResponse<List<String>> constraintViolationException(ConstraintViolationException e) {
+    public ResponseEntity<CustomResponse<List<String>>> constraintViolationException(ConstraintViolationException e) {
 
         log.error(Arrays.toString(e.getStackTrace()));
         List<String> message = e.getConstraintViolations().stream().map(ConstraintViolation::getMessage).toList();
 
-        return CustomResponse.onFailure(GeneralErrorCode.BAD_REQUEST_400, message);
+        return ResponseEntity.badRequest().body(CustomResponse.onFailure(GeneralErrorCode.BAD_REQUEST_400, message));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public CustomResponse<Map<String, String>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<CustomResponse<Map<String, String>>> methodArgumentNotValidException(MethodArgumentNotValidException e) {
         Map<String, String> error = new HashMap<>();
         e.getBindingResult().getFieldErrors().forEach(field -> error.put(field.getField(), field.getDefaultMessage()));
 
         log.error(Arrays.toString(e.getStackTrace()));
-        return CustomResponse.onFailure(GeneralErrorCode.BAD_REQUEST_400, error);
+        return ResponseEntity.badRequest().body(CustomResponse.onFailure(GeneralErrorCode.BAD_REQUEST_400, error));
     }
 
     @ExceptionHandler(GeneralException.class)
-    public CustomResponse<String> generalException(GeneralException e) {
+    public ResponseEntity<CustomResponse<String>> generalException(GeneralException e) {
         BaseErrorCode code = e.getCode();
 
         log.error(Arrays.toString(e.getStackTrace()));
 
-        return CustomResponse.onFailure(code);
+        return ResponseEntity.status(code.getStatus()).body(CustomResponse.onFailure(code));
     }
 
     @ExceptionHandler(Exception.class)
-    public CustomResponse<String> exception(Exception e) {
+    public ResponseEntity<CustomResponse<String>> exception(Exception e) {
 
         log.error(Arrays.toString(e.getStackTrace()));
 
-        return CustomResponse.onFailure(GeneralErrorCode.INTERNAL_SERVER_ERROR_500);
+        return ResponseEntity.internalServerError().body(CustomResponse.onFailure(GeneralErrorCode.INTERNAL_SERVER_ERROR_500));
     }
 }
